d951b9c0f62098be67c838a3e8a0df7cdfc8923e
aman <amanbek94@gmail.com>
2021-03-26 16:34:33 +0600

    parse all xml files

--- app/Http/Controllers/LandController.php
+++ app/Http/Controllers/LandController.php
@@ -25,45 +25,48 @@ class LandController extends Controller
 
     public function from_xml_file(){
     	try {
-    		$fileContents = file_get_contents("/var/www/aisgzk_data/public/test.xml");
-	        $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
-	        $fileContents = trim(str_replace('"', "'", $fileContents));
-	        $clean_xml = str_ireplace(['SOAP-ENV:', 'SOAP:', 'shep1:', 'shep2:'], '', $fileContents);
-			$simpleXml = simplexml_load_string($clean_xml);
-			$insert = 0;
-			$update = 0;
-			foreach ($simpleXml->Body->SendMessage->request->requestData->data->Data as $item) {
-				$CadNumber = trim(str_replace('-', '', $item->GzkObject->CadNumber));
-				$feature = $this->get_feature($item);
-				if(Land::where('CadNumber', $CadNumber)->count() > 0){
-					$land = Land::where('CadNumber', $CadNumber)->first();
-					$land->feature = $feature;
-		    		$land->ActualDate = $item->ActualDate;
-		    		$sd_arr = [];
-					foreach ($item->SemanticData as $sd){
-						$sd_arr[] = $sd;
+    		$files = array_diff(scandir('/var/www/aisgzk_data/public/xml_files'), array('.', '..'));
+    		foreach ($files as $file) {
+    			$fileContents = file_get_contents("/var/www/aisgzk_data/public/xml_files/".$file);
+		        $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
+		        $fileContents = trim(str_replace('"', "'", $fileContents));
+		        $clean_xml = str_ireplace(['SOAP-ENV:', 'SOAP:', 'shep1:', 'shep2:'], '', $fileContents);
+				$simpleXml = simplexml_load_string($clean_xml);
+				$insert = 0;
+				$update = 0;
+				foreach ($simpleXml->Body->SendMessage->request->requestData->data->Data as $item) {
+					$CadNumber = trim(str_replace('-', '', $item->GzkObject->CadNumber));
+					$feature = $this->get_feature($item);
+					if(Land::where('CadNumber', $CadNumber)->count() > 0){
+						$land = Land::where('CadNumber', $CadNumber)->first();
+						$land->feature = $feature;
+			    		$land->ActualDate = $item->ActualDate;
+			    		$sd_arr = [];
+						foreach ($item->SemanticData as $sd){
+							$sd_arr[] = $sd;
+						}
+			    		$land->SemanticData = json_encode($sd_arr);
+			    		$land->Geometry = json_encode($item->Geometry);
+			    		$land->GzkObject = json_encode($item->GzkObject);
+			    		$land->save();
+						$update++;
+					}else{
+						$land = new Land();
+						$land->CadNumber = $CadNumber;
+						$land->feature = $feature;
+			    		$land->ActualDate = $item->ActualDate;
+			    		$sd_arr = [];
+						foreach ($item->SemanticData as $sd){
+							$sd_arr[] = $sd;
+						}
+			    		$land->SemanticData = json_encode($sd_arr);
+			    		$land->Geometry = json_encode($item->Geometry);
+			    		$land->GzkObject = json_encode($item->GzkObject);
+			    		$land->save();
+			    		$insert++;
 					}
-		    		$land->SemanticData = json_encode($sd_arr);
-		    		$land->Geometry = json_encode($item->Geometry);
-		    		$land->GzkObject = json_encode($item->GzkObject);
-		    		$land->save();
-					$update++;
-				}else{
-					$land = new Land();
-					$land->CadNumber = $CadNumber;
-					$land->feature = $feature;
-		    		$land->ActualDate = $item->ActualDate;
-		    		$sd_arr = [];
-					foreach ($item->SemanticData as $sd){
-						$sd_arr[] = $sd;
-					}
-		    		$land->SemanticData = json_encode($sd_arr);
-		    		$land->Geometry = json_encode($item->Geometry);
-		    		$land->GzkObject = json_encode($item->GzkObject);
-		    		$land->save();
-		    		$insert++;
 				}
-			}
+    		}
     		return response()->json(['insert' => $insert, 'update' => $update], 200);
     	} catch (Exception $e) {
     		return response()->json(['message' => $e->getMessage()], 500);
